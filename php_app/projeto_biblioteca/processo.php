ef74e5925e7b31786c56cfbcf961316a9cc2997a
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-11 19:48:14 -0300

    atualizaçã: criação variavel conn para ligamento com arquivo conexão.php

---
+++ php_app/projeto_biblioteca/processo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dados do usuario</title>
+</head>
+
+<body>
+    <?php
+
+    include_once("conexão.php");
+
+    $user_name = $_POST["nome"] ? $_POST["nome"] : false;
+    $email = $_POST["email"] ? $_POST["email"] : false;
+
+    // echo "nome do usuario: " . $user_name . " <br/>";
+    // echo "email: " . $email . " <br/>";
+
+    $result_costumers = "insert into costumers (name, email, created) values ('$name', '$email', NOW())";
+    $resultado_costumers = mysqli_query($conn, $result_costumers);
+
+    ?>
+    <a href="index.php">voltar</a>
+</body>
+
+</html>
