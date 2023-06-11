15cb489a5e728e37de85253405d766e2da77d9ed
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-11 19:49:04 -0300

    atualização: correção corpo do código

---
+++ php_app/projeto_biblioteca/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+</head>
+
+<body>
+    <form action="processo.php" method="post">
+        <h2>cadastro de usuário</h2>
+        <label for="l.n">nome</label>
+        <input type="text" name="nome" id="l.n" /> <br />
+        <label for="l.e">email</label>
+        <input type="email" name="email" id="l.e" /> <br />
+        <input type="submit" name="envio" id="submit" />
+    </form>
+</body>
+
+</html>
