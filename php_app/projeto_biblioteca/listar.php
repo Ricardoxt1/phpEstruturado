a37781f1ad45094a4bc55f2bedfa3484985c2f82
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-12 19:35:33 -0300

    metodo get all: requisição de todos os costumers

--- php_app/projeto_biblioteca/listar.php
+++ php_app/projeto_biblioteca/listar.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include_once("./conexão.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -9,6 +10,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Biblioteca</title>
+    <a href="./cadastrar.php">cadastrar</a>
+    <a href="./listar.php">listar</a>
 </head>
 
 <body>
@@ -20,10 +23,25 @@ session_start();
             unset($_SESSION['msg']);
         }
 
+        //inserir paginação
+        $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);  
+        $pagina = (!empty($pagina_atual) ? $pagina_atual : 1);
+
+        //setar quantidade de itens de pagina 
+
+        //seleção de todos costumers por id, nome e email 
+        $result_costumers = "SELECT * FROM costumers";
+        $total_costumers = mysqli_query($conn, $result_costumers);
+        while ($costumers = mysqli_fetch_assoc($total_costumers)){
+            echo "Id: ". $costumers['id'] . "<br>";
+            echo "name: ". $costumers['name'] . "<br>";
+            echo "email: ". $costumers['email'] . "<br><hr>";
+        }
+
         ?>
     </div>
-    <form action="./processo.php" method="get">
-        
+    
+    
     </form>
 </body>
 
