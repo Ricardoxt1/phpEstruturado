0bb21becdd74869805a87413f016ec208c8686dd
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-12 19:37:00 -0300

    adição: inserido aviso de cadastro

--- php_app/projeto_biblioteca/processo.php
+++ php_app/projeto_biblioteca/processo.php
@@ -1,34 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+include_once("conexão.php");
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dados do usuario</title>
-</head>
+$user_name = $_POST["nome"] ? $_POST["nome"] : false;
+$email = $_POST["email"] ? $_POST["email"] : false;
 
-<body>
-    <?php
+$result_costumers = "insert into costumers (name, email, created) values ('$user_name', '$email', NOW())";
+$resultado_costumers = mysqli_query($conn, $result_costumers);
 
-    include_once("conexão.php");
-
-    $user_name = $_POST["nome"] ? $_POST["nome"] : false;
-    $email = $_POST["email"] ? $_POST["email"] : false;
-
-    // echo "nome do usuario: " . $user_name . " <br/>";
-    // echo "email: " . $email . " <br/>";
-
-    $result_costumers = "insert into costumers (name, email, created) values ('$name', '$email', NOW())";
-    $resultado_costumers = mysqli_query($conn, $result_costumers);
-
-    if (mysqli_insert_id($conn)) {
-        header("Location: cadastrar.php");
-    } else {
-        header("Location: cadastrar.php");
-    }
-    ?>
-    <a href="./cadastrar.php">voltar</a>
-</body>
-
-</html>
+if (mysqli_insert_id($conn)) {
+    $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
+    header("Location:cadastrar.php");
+} else {
+    $_SESSION['msg'] = "<p style='color:green;'>Cadastro não foi realizado com sucesso.</p>";
+    header("Location:cadastrar.php");
+}
+?>
